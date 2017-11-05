73c945f7552078d743f880a95a3acd51cdb3680d
Ruan Andrade <ruan.moa@gmail.com>
2017-11-05 18:50:10 -0200

    Adding staticfiles in AWS-S3

--- eventex/settings.py
+++ eventex/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'eventex.core',
+    'storages',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -96,8 +97,15 @@ USE_L10N = True
 USE_TZ = True
 
 
+##Amazon
+AWS_DEFAULT_ACL = ''
+AWS_STORAGE_BUCKET_NAME = config('S3_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
